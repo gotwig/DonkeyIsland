ee5c2e0bad216f20e3d8accb124d696e8414cbf7
Eduard Gotwig <gotwig@mail.com>
2016-02-09 16:42:51 +0100

    fix windows build, include dependencies

--- thirdsemestergame/main.cpp
+++ thirdsemestergame/main.cpp
@@ -32,11 +32,11 @@ void update(sf::Time elapsed)
 
 int main()
 {
-	const sf::Vector2i screenDimensions(800,600);
-  
-        FloatRect playerMoveSpace = sf::FloatRect(100,100,screenDimensions.x-100,screenDimensions.y-320);
+	const sf::Vector2i screenDimensions(800, 600);
 
-        
+	FloatRect playerMoveSpace = sf::FloatRect(100, 100, screenDimensions.x - 100, screenDimensions.y - 320);
+
+
 	RenderWindow window(VideoMode(screenDimensions.x, screenDimensions.y), "Donkey Island", Style::Titlebar | Style::Close);
 
 	window.setFramerateLimit(60);
@@ -49,7 +49,7 @@ int main()
 
 	Texture PlayerTex;
 	AnimatedSprite PlayerImage;
-	
+
 	sf::Time actionTimer;
 
 	GameScene* loadedLevel = new LakeGrass();
@@ -75,48 +75,48 @@ int main()
 	Quests* playerQuests = new Quests(loadedLevel);
 
 
-		if (!PlayerTex.loadFromFile("donkeyMovement.png")) {
-		    std::cout << "Failed to load player spritesheet!" << std::endl;
-		    return 1;
-		}
-		
-		Animation walkingAnimationRight;
-		walkingAnimationRight.setSpriteSheet(PlayerTex);
-		walkingAnimationRight.addFrame(sf::IntRect(200, 0, 200, 200));
-		walkingAnimationRight.addFrame(sf::IntRect(400, 0, 200, 200));
-		walkingAnimationRight.addFrame(sf::IntRect(600, 0, 200, 200));
-		walkingAnimationRight.addFrame(sf::IntRect( 0, 0, 200, 200));
+	if (!PlayerTex.loadFromFile("donkeyMovement.png")) {
+		std::cout << "Failed to load player spritesheet!" << std::endl;
+		return 1;
+	}
 
-		Animation walkingAnimationLeft;
-		walkingAnimationLeft.setSpriteSheet(PlayerTex);
-		walkingAnimationLeft.addFrame(sf::IntRect(200, 200, 200, 200));
-		walkingAnimationLeft.addFrame(sf::IntRect(400, 200, 200, 200));
-		walkingAnimationLeft.addFrame(sf::IntRect(600, 200, 200, 200));
-		walkingAnimationLeft.addFrame(sf::IntRect( 0, 200, 200, 200));
-                
-                Animation walkingAnimationIdle;
-		walkingAnimationIdle.setSpriteSheet(PlayerTex);
-		walkingAnimationIdle.addFrame(sf::IntRect(200, 400, 200, 200));
-		walkingAnimationIdle.addFrame(sf::IntRect(400, 400, 200, 200));
-		walkingAnimationIdle.addFrame(sf::IntRect(600, 400, 200, 200));
-		walkingAnimationIdle.addFrame(sf::IntRect( 0, 400, 200, 200));
-		
-		Animation* currentAnimation = &walkingAnimationLeft;
-	
-		PlayerImage = AnimatedSprite(sf::seconds(0.1), true, false);
-		PlayerImage.setPosition(sf::Vector2f(screenDimensions / 3));
-		
-		sf::Clock frameClock;
+	Animation walkingAnimationRight;
+	walkingAnimationRight.setSpriteSheet(PlayerTex);
+	walkingAnimationRight.addFrame(sf::IntRect(200, 0, 200, 200));
+	walkingAnimationRight.addFrame(sf::IntRect(400, 0, 200, 200));
+	walkingAnimationRight.addFrame(sf::IntRect(600, 0, 200, 200));
+	walkingAnimationRight.addFrame(sf::IntRect(0, 0, 200, 200));
+
+	Animation walkingAnimationLeft;
+	walkingAnimationLeft.setSpriteSheet(PlayerTex);
+	walkingAnimationLeft.addFrame(sf::IntRect(200, 200, 200, 200));
+	walkingAnimationLeft.addFrame(sf::IntRect(400, 200, 200, 200));
+	walkingAnimationLeft.addFrame(sf::IntRect(600, 200, 200, 200));
+	walkingAnimationLeft.addFrame(sf::IntRect(0, 200, 200, 200));
+
+	Animation walkingAnimationIdle;
+	walkingAnimationIdle.setSpriteSheet(PlayerTex);
+	walkingAnimationIdle.addFrame(sf::IntRect(200, 400, 200, 200));
+	walkingAnimationIdle.addFrame(sf::IntRect(400, 400, 200, 200));
+	walkingAnimationIdle.addFrame(sf::IntRect(600, 400, 200, 200));
+	walkingAnimationIdle.addFrame(sf::IntRect(0, 400, 200, 200));
+
+	Animation* currentAnimation = &walkingAnimationLeft;
+
+	PlayerImage = AnimatedSprite(sf::seconds(0.1), true, false);
+	PlayerImage.setPosition(sf::Vector2f(screenDimensions / 3));
+
+	sf::Clock frameClock;
 
 
-		float speed = 160.f;
-        bool noKeyWasPressed = true;
-        
-        
+	float speed = 160.f;
+	bool noKeyWasPressed = true;
+
+
 	// Our GAMELOOP starts here
 	while (window.isOpen()) {
 		Event event;
-		while (window.pollEvent(event)){
+		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed) {
 				window.close();
 			}
@@ -130,49 +130,49 @@ int main()
 		}
 
 		sf::Time frameTime = frameClock.restart();
-                
-        // if a key was pressed set the correct animation and move correctly
-        sf::Vector2f movement(0.f, 0.f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            movement.y -= speed;
-            noKeyWasPressed = false;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            movement.y += speed;
-            noKeyWasPressed = false;
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            currentAnimation = &walkingAnimationLeft;
-            movement.x -= speed;
-            noKeyWasPressed = false;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            currentAnimation = &walkingAnimationRight;
-            movement.x += speed;
-            noKeyWasPressed = false;
-        }
-        PlayerImage.play(*currentAnimation);
-        PlayerImage.move(movement * frameTime.asSeconds());
 
-        // if no key was pressed stop the animation
-        if (noKeyWasPressed)
-        {
-            //PlayerImage.stop();
-            PlayerImage.setFrameTime(sf::seconds(0.4));
-            
-            currentAnimation = &walkingAnimationIdle;
-        }
-        if (!noKeyWasPressed){
-            PlayerImage.setFrameTime(sf::seconds(0.1)); 
-        }
-        noKeyWasPressed = true;
+		// if a key was pressed set the correct animation and move correctly
+		sf::Vector2f movement(0.f, 0.f);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			movement.y -= speed;
+			noKeyWasPressed = false;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			movement.y += speed;
+			noKeyWasPressed = false;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			currentAnimation = &walkingAnimationLeft;
+			movement.x -= speed;
+			noKeyWasPressed = false;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			currentAnimation = &walkingAnimationRight;
+			movement.x += speed;
+			noKeyWasPressed = false;
+		}
+		PlayerImage.play(*currentAnimation);
+		PlayerImage.move(movement * frameTime.asSeconds());
 
-        // update AnimatedSprite
-        PlayerImage.update(frameTime);		
+		// if no key was pressed stop the animation
+		if (noKeyWasPressed)
+		{
+			//PlayerImage.stop();
+			PlayerImage.setFrameTime(sf::seconds(0.4));
+
+			currentAnimation = &walkingAnimationIdle;
+		}
+		if (!noKeyWasPressed) {
+			PlayerImage.setFrameTime(sf::seconds(0.1));
+		}
+		noKeyWasPressed = true;
+
+		// update AnimatedSprite
+		PlayerImage.update(frameTime);
 		playerQuests->checkQuests(draggeditem, playerInventory, PlayerImage);
 
 		window.clear();
@@ -180,14 +180,14 @@ int main()
 		if (loadedLevel) {
 			window.draw(loadedLevel->bgSprite);
 		}
-		
+
 		window.draw(MenuBackground);
 
 		for (auto && entity : loadedLevel->currentEntities) {
 			window.draw(entity.Sprite);
 		}
 
-		if (playerQuests->triggerShowText){
+		if (playerQuests->triggerShowText) {
 
 			if (playerQuests->clock.getElapsedTime().asSeconds() < 10)
 				window.draw(playerQuests->talkText);
@@ -217,26 +217,21 @@ int main()
 		loadedLevel->Update(playerQuests->currentQuestId);
 
 
-		
+
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
 			sf::Vector2i localPosition = sf::Mouse::getPosition(window);
 			sf::Vector2f worldPos = window.mapPixelToCoords(localPosition);
-			if (localPosition.y < 380 && !dragging){
-                            if (playerMoveSpace.contains(PlayerImage.getPosition()) ){
-                                Target1.x = worldPos.x -100;
-				Target1.y = worldPos.y -100;
-                                
-                            }
-                            
-				
+			if (localPosition.y < 380 && !dragging) {
+					Target1.x = worldPos.x - 100;
+					Target1.y = worldPos.y - 100;
 			}
 
 			if (localPosition.y > 380 && !dragging) {
 
 				for (auto && item : playerInventory->currentItems) {
-					if (localPosition.y < item.itemIconSprite.getPosition().y + item.itemIconSprite.getGlobalBounds().height && localPosition.y   > item.itemIconSprite.getPosition().y && localPosition.x  < item.itemIconSprite.getPosition().x + item.itemIconSprite.getGlobalBounds().width && localPosition.x  > item.itemIconSprite.getPosition().x  ) {
-						item.itemIconSprite.setPosition(localPosition.x-32, localPosition.y-32);
+					if (localPosition.y < item.itemIconSprite.getPosition().y + item.itemIconSprite.getGlobalBounds().height && localPosition.y   > item.itemIconSprite.getPosition().y && localPosition.x  < item.itemIconSprite.getPosition().x + item.itemIconSprite.getGlobalBounds().width && localPosition.x  > item.itemIconSprite.getPosition().x) {
+						item.itemIconSprite.setPosition(localPosition.x - 32, localPosition.y - 32);
 						dragging = true;
 						draggeditem = &item;
 
@@ -247,9 +242,9 @@ int main()
 			}
 
 
-			if (draggeditem) { 
+			if (draggeditem) {
 				if (dragging) {
-				
+
 					draggeditem->itemIconSprite.setPosition(localPosition.x - 32, localPosition.y - 32);
 				}
 
@@ -268,7 +263,7 @@ int main()
 
 			//PlayerImage.move(Target1.x,Target1.y);
 
-			if (!clicked){
+			if (!clicked) {
 				PlayerImage.setPosition(Target1.x, Target1.y);
 			}
 
@@ -279,6 +274,6 @@ int main()
 	}
 
 
-return 0;
+	return 0;
 }
 
