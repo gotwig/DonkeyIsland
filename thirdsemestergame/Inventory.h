8c45aa3c278bcddf6dd6a64654578b6de2f901fd
Eduard Gotwig <gotwig@mail.com>
2016-02-22 20:26:56 +0100

    Update Inventory.h

--- thirdsemestergame/Inventory.h
+++ thirdsemestergame/Inventory.h
@@ -11,10 +11,16 @@ public:
 		sf::Texture itemIconTex;
 	};
 	
+	// dynamic array for the currentItems in the player inventory
 	std::vector< struct item > currentItems;
 
+	// keep the inventory state up-to-date
 	void refreshItems();
+	
+	//add new items by id
 	void addItem(int id);
+	
+	//remove new items by id
 	void removeItem(int id);
 
 };
