c1df1c241e5071ebe6e08929648ea096c24caa5a
Eduard Gotwig <gotwig@mail.com>
2016-02-22 20:49:05 +0100

    Update gamescene.h

--- thirdsemestergame/gamescene.h
+++ thirdsemestergame/gamescene.h
@@ -1,5 +1,6 @@
-#pragma once
+// This class is the base for other level classes, to create game scenes
 
+#pragma once
 
 #include <SFML/Graphics.hpp>
 #include <string>
@@ -32,16 +33,22 @@ public:
 public:
 	GameScene();
 
+	//Gameloop for each level
 	virtual void Update(int currentQuestid);
 
+	//Set levelname, for the correct entry finding in quests.xml
 	void setName(const std::string &Name);
 
+	//set the level background
 	void setBgPath(const std::string &bgPath);
 
+	//set the level background sound
 	void setLvlSound(const std::string &audioPath);
 
+	//add entities to the level scene
 	void addEnt(int id, std::string texPath, float x, float y);
 
+	//remove entities from the level scene
 	void delEnt(int id);
 };
 
